65e3a7734dbac4ecb5f1a37bef88579a689ce9c7
Anoop Kumar <anoopkumar220481@gmail.com>
2022-04-05 23:00:44 +0530

    Changes to footer

--- components/footer.js
+++ components/footer.js
@@ -1,17 +1,14 @@
-import Container from "./container";
-import { FaCopyright } from "react-icons/fa";
-
 export default function Footer() {
   return (
     <footer className="from-neutral-600/20 to-gray-600/50 bg-gradient-to-tr mx-5 mt-auto">
-      <Container>
-        <div className="flex justify-end m-1">
-          <h1>
-            <FaCopyright className="h-full mx-1 opacity-30" />
-          </h1>
-          <h1 className="text-lg font-thin opacity-30 text-white">Neographer</h1>
+      <div className="flex flex-row w-full">
+        <div className="justify-start grow m-2">
+          <p className="text-sm font-thin opacity-30 text-white">Designed & Developed by Neographer.</p>
         </div>
-      </Container>
+        <div className="flex justify-end m-2">
+          <h1 className="text-sm font-thin opacity-30 text-white">© {new Date().getFullYear()} Neographer. All Rights Reserved.</h1>
+        </div>
+      </div>
     </footer>
   );
 }
