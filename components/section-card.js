f9de4aab37737e4dcb81d3b922b38851a581a147
Anoop Kumar <anoopkumar220481@gmail.com>
2022-04-21 22:17:33 +0530

    Adding shadow

--- components/section-card.js
+++ components/section-card.js
@@ -4,7 +4,7 @@ export default function SectionCard({ section }) {
       <h1 className="text-lg uppercase tracking-widest border-b-2 m-2 text-lightred/80">{section.name}</h1>
       <div className="grid lg:grid-flow-row grid-cols-2 lg:grid-cols-4 lg:gap-2">
         {section.data.map((item, index) => (
-          <div key={index} className="m-2 p-2 hover:bg-lightred/5">
+          <div key={index} className="shadow-lg m-2 p-2 hover:bg-lightred/5">
             <h1 className="font-bold mx-1">{item.name}</h1>
             <h1 className="font-thin ml-1">{item.location}</h1>
             <h1 className="font-thin ml-1">
